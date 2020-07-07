2fbc7da7c3b953bcc34be2a45aacce5b5b5bc933
knatesan <krishnamoorthy.natesan@us.fujitsu.com>
2020-07-07 04:53:35 -0500

    update

--- install_bashrc.sh
+++ install_bashrc.sh
@@ -6,8 +6,8 @@ if [[ -z "${ENV_CONFIG}" ]]; then
 fi
 
 echo 'source $ENV_CONFIG/.bash_config/bashrcs/bashrc.sh
-source $ENV_CONFIG/.bash_config/bashrcs/bashrc_prj.sh
 source $ENV_CONFIG/.bash_config/bashrcs/bash_alias.sh
-source $ENV_CONFIG/.bash_config/bashrcs/bash_alias_prj.sh' >> ~/.bashrc
+source $ENV_CONFIG/.bash_config/project/bashrcs/bashrc.sh
+source $ENV_CONFIG/.bash_config/project/bashrcs/bash_alias.sh' >> ~/.bashrc
 
 echo "Installed the BASH configuration successfully!"
